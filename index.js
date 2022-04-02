c603a4cda0d5bd8ec620e632db6ecf3c9942c062
TomKrok1 <91782249+trak2022z@users.noreply.github.com>
2022-04-02 22:25:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
